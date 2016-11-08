1a03bde814342ccc67f04b26d230515544b46e3a
racheltstevens <racheltstevens@gmail.com>
2016-11-08 16:45:44 -0500

    Add Shape starter code
    Update for cohort 015

---
+++ lib/shape.rb
@@ -0,0 +1,13 @@
+class Shape
+  attr_reader :num_sides
+  attr_accessor :color, :side_length
+
+  def initialize(num_sides, side_length)
+    @num_sides = num_sides
+    @side_length = side_length
+  end
+
+  def calculate_area
+    (@num_sides * @side_length**2) / (4 * Math.tan(Math::PI/@num_sides))
+  end
+end
